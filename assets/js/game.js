82ac73f2c71a6a32d32a12408cea94fa5ba2dfc8
lssdavies <91108369+IslandBoyL@users.noreply.github.com>
2021-10-21 20:53:59 -0700

    created variable playerName to store user's robot name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+//This ask the user for their robots name
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName + " is your gladiator's name!");
+
+//this creates a fundtion names "fight"
+
+function fight()    {
+    window.alert("The fight has begun!");
+}
+
+//fight();
